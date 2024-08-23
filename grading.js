751f80bfc0ec6083fa01dcd83104b72fd3abe9a5
Muhammad Hanzala Afaq <160254388+HanzalaAq@users.noreply.github.com>
2024-08-23 17:22:25 +0500

    Add files via upload

---
+++ grading.js
@@ -0,0 +1,18 @@
+let marks = 89.99;
+
+if (marks >= 90 ){
+    console.log("Congrats, You have secured A+ Grade")
+}
+else if(marks>=75 && marks<90){
+ console.log("You have secured A Grade")
+}
+else if(marks>=60 && marks<75){
+ console.log("You have secured B Grade")
+}
+else if(marks>=50 && marks<60){
+ console.log("You have secured C Grade")
+ 
+}
+else{
+    console("You have failed ")
+}
